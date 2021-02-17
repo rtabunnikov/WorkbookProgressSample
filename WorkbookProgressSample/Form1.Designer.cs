8ef55be1485e905086ab4f706e4d7ba0d6cb09ec
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2021-02-17 17:19:27 +0300

    Add project files.

---
+++ WorkbookProgressSample/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WorkbookProgressSample {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbLoad = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbExport = new System.Windows.Forms.ProgressBar();
+            this.butRun = new System.Windows.Forms.Button();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Loading workbook...";
+            // 
+            // pbLoad
+            // 
+            this.pbLoad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbLoad.Location = new System.Drawing.Point(30, 51);
+            this.pbLoad.Name = "pbLoad";
+            this.pbLoad.Size = new System.Drawing.Size(507, 23);
+            this.pbLoad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exporting to PDF...";
+            // 
+            // pbExport
+            // 
+            this.pbExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbExport.Location = new System.Drawing.Point(30, 134);
+            this.pbExport.Name = "pbExport";
+            this.pbExport.Size = new System.Drawing.Size(507, 23);
+            this.pbExport.TabIndex = 3;
+            // 
+            // butRun
+            // 
+            this.butRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butRun.Location = new System.Drawing.Point(438, 200);
+            this.butRun.Name = "butRun";
+            this.butRun.Size = new System.Drawing.Size(99, 34);
+            this.butRun.TabIndex = 4;
+            this.butRun.Text = "Run";
+            this.butRun.UseVisualStyleBackColor = true;
+            this.butRun.Click += new System.EventHandler(this.butRun_Click);
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDone.ForeColor = System.Drawing.Color.Red;
+            this.lblDone.Location = new System.Drawing.Point(27, 209);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(50, 17);
+            this.lblDone.TabIndex = 5;
+            this.lblDone.Text = "Done!";
+            this.lblDone.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 273);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.butRun);
+            this.Controls.Add(this.pbExport);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbLoad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Workbook operations progress sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbLoad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pbExport;
+        private System.Windows.Forms.Button butRun;
+        private System.Windows.Forms.Label lblDone;
+    }
+}
+
---
+++ WorkbookProgressSample/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.Spreadsheet;
+
+namespace WorkbookProgressSample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void butRun_Click(object sender, EventArgs e) {
+            lblDone.Visible = false;
+            pbLoad.Value = 0;
+            pbExport.Value = 0;
+            using (Workbook workbook = new Workbook()) {
+                await workbook.LoadDocumentAsync("document.xlsx",
+                    new Progress<int>((progress) => {
+                        pbLoad.Value = progress;
+                        pbLoad.Refresh();
+                    }));
+                await workbook.ExportToPdfAsync("result.pdf",
+                    new Progress<int>((progress) => {
+                        pbExport.Value = progress;
+                        pbExport.Refresh();
+                    }));
+            }
+            lblDone.Visible = true;
+        }
+    }
+}
